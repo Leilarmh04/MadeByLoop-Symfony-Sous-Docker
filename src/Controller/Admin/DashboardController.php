dc7ebed00f5e80f0ab3768fa75afd054edd41775
-m <-m>
2026-04-20 17:29:37 +0200

    feat: add EasyAdmin dashboard with CRUD for products, users, categories and orders

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,7 +18,13 @@ class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return $this->render('@EasyAdmin/page/content.html.twig');
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect(
+            $adminUrlGenerator
+                ->setController(ProductCrudController::class)
+                ->generateUrl()
+        );
     }
 
     public function configureDashboard(): Dashboard
@@ -28,26 +35,24 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
 
         yield MenuItem::section('Utilisateurs');
-        yield MenuItem::linkToUrl('Utilisateurs', 'fa fa-users', $this->generateUrl('admin', [
-            'crudControllerFqcn' => UserCrudController::class,
-            'crudAction' => 'index',
-        ]));
+        yield MenuItem::linkToUrl('Utilisateurs', 'fa fa-users',
+            $adminUrlGenerator->setController(UserCrudController::class)->generateUrl()
+        );
 
         yield MenuItem::section('Boutique');
-        yield MenuItem::linkToUrl('Produits', 'fa fa-box', $this->generateUrl('admin', [
-            'crudControllerFqcn' => ProductCrudController::class,
-            'crudAction' => 'index',
-        ]));
-        yield MenuItem::linkToUrl('Catégories', 'fa fa-tags', $this->generateUrl('admin', [
-            'crudControllerFqcn' => CategoryCrudController::class,
-            'crudAction' => 'index',
-        ]));
-        yield MenuItem::linkToUrl('Commandes', 'fa fa-shopping-cart', $this->generateUrl('admin', [
-            'crudControllerFqcn' => OrderCrudController::class,
-            'crudAction' => 'index',
-        ]));
+        yield MenuItem::linkToUrl('Produits', 'fa fa-box',
+            $adminUrlGenerator->setController(ProductCrudController::class)->generateUrl()
+        );
+        yield MenuItem::linkToUrl('Catégories', 'fa fa-tags',
+            $adminUrlGenerator->setController(CategoryCrudController::class)->generateUrl()
+        );
+        yield MenuItem::linkToUrl('Commandes', 'fa fa-shopping-cart',
+            $adminUrlGenerator->setController(OrderCrudController::class)->generateUrl()
+        );
     }
 }
